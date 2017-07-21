c739c26589bcb1fa8b5105df854315e49f6aa217
Xiangxing Liu <xiangxingliu@XiangxingdeMacBook-Pro.local>
2017-07-21 00:47:47 -0500

    initial

---
+++ src/lab2/Driver.java
@@ -0,0 +1,48 @@
+package lab2;
+
+
+
+import java.util.Vector;
+
+public class Driver {
+    
+    public static void main(String[] args) {
+      
+	if (args.length == 0) {
+	    System.err.println("usage: java Driver <filename> -t <list of transmission ranges>");
+            System.exit(1);
+	}
+
+	Vector<Double> transmissionRange = new Vector<Double>(0);
+
+	String filename = args[0];
+	boolean flag_t = false;
+        for (int i=1; i<args.length; i++) {
+            if(args[i].equals("-t")) {
+		flag_t = true;
+            } else if(flag_t == true) {
+		transmissionRange.add(Double.parseDouble(args[i]));
+	    }
+            else {
+                System.err.printf("Unknown option: %s\n", args[i]);
+		System.exit(1);
+            }
+        }
+        
+        Program2 network = new Program2(filename);
+        System.out.println("---------------------------");
+        System.out.println("Results for the input graph");
+        System.out.println("---------------------------");
+        System.out.println("");
+        for (int r = 0; r < transmissionRange.size(); r++) {
+            System.out.println("Transmission Range = " + transmissionRange.get(r) + " meters.");
+            System.out.println("");
+            network.setTransmissionRange(transmissionRange.get(r));
+            network.gpsrAllPairs(false);
+            network.dijkstraLatencyAllPairs(false);
+            network.dijkstraHopsAllPairs(false);
+        }
+    }
+    
+}
+
---
+++ src/lab2/Program2.java
@@ -0,0 +1,77 @@
+/*
+ * Name: Xiangxing Liu
+ * EID: xl5587
+ */
+
+package lab2;
+
+
+import java.util.Vector;
+
+/* Your solution goes in this file.
+ *
+ * Please do not modify the other files we have provided for you, as we will use
+ * our own versions of those files when grading your project. You are
+ * responsible for ensuring that your solution works with the original version
+ * of all the other files we have provided for you.
+ * 
+ * That said, please feel free to add additional files and classes to your
+ * solution, as you see fit. We will use ALL of your additional files when
+ * grading your solution.
+ */
+
+public class Program2 extends VertexNetwork {
+    /* DO NOT FORGET to add a graph representation and 
+       any other fields and/or methods that you think 
+       will be useful. 
+       DO NOT FORGET to modify the constructors when you 
+       add new fields to the Program2 class. */
+    
+    Program2() {
+        super();
+    }
+    
+    Program2(String locationFile) {
+        super(locationFile);
+    }
+    
+    Program2(String locationFile, double transmissionRange) {
+        super(locationFile, transmissionRange);
+    }
+    
+    Program2(double transmissionRange, String locationFile) {
+        super(transmissionRange, locationFile);
+    }
+
+    public Vector<Vertex> gpsrPath(int sourceIndex, int sinkIndex) {
+        /* This method returns a path from a source at location sourceIndex 
+           and a sink at location sinkIndex using the GPSR algorithm. An empty 
+           path is returned if the GPSR algorithm fails to find a path. */
+        /* The following code is meant to be a placeholder that simply 
+           returns an empty path. Replace it with your own code that 
+           implements the GPSR algorithm. */
+        return new Vector<Vertex>(0);
+    }
+    
+    public Vector<Vertex> dijkstraPathLatency(int sourceIndex, int sinkIndex) {
+        /* This method returns a path (shortest in terms of latency) from a source at
+           location sourceIndex and a sink at location sinkIndex using Dijkstra's algorithm.
+           An empty path is returned if Dijkstra's algorithm fails to find a path. */
+        /* The following code is meant to be a placeholder that simply 
+           returns an empty path. Replace it with your own code that 
+           implements Dijkstra's algorithm. */
+        return new Vector<Vertex>(0);
+    }
+    
+    public Vector<Vertex> dijkstraPathHops(int sourceIndex, int sinkIndex) {
+        /* This method returns a path (shortest in terms of hops) from a source at
+           location sourceIndex and a sink at location sinkIndex using Dijkstra's algorithm.
+           An empty path is returned if Dijkstra's algorithm fails to find a path. */
+        /* The following code is meant to be a placeholder that simply 
+           returns an empty path. Replace it with your own code that 
+           implements Dijkstra's algorithm. */
+        return new Vector<Vertex>(0);
+    }
+    
+}
+
